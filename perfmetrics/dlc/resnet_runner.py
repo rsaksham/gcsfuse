6e1059c403db9fe59d8e60ebaa1d53b8805c30ff
Ayush Sethi <ayushsethi@google.com>
2022-12-21 08:57:08 +0000

    Increasing epochs to 5000

--- perfmetrics/dlc/resnet_runner.py
+++ perfmetrics/dlc/resnet_runner.py
@@ -102,7 +102,7 @@ model, eval_logs = tfm.core.train_lib.run_experiment(
     params=exp_config,
     model_dir=model_dir,
     run_post_eval=True,
-    epochs=1000,
+    epochs=5000,
     clear_kernel_cache=True)
 # os.system("sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'")
 new_time = time.perf_counter()
